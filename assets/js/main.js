dbb8923aec92a4e5235144fa4fe691d0c9aee7e2
ChemieAi <kzlyburak@gmail.com>
2025-06-11 15:46:39 +0300

    fixed
    eksik dosyalar eklendi

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,4 +14,32 @@ window.onload = function () {
 		menu_btn.classList.toggle('is-active');
 		mobile_menu.classList.toggle('is-active');
 	});
+
+	const navbar = document.querySelector('.navbar');
+	
+
+	// Close the navbar menu when a menu item is clicked
+	menu_btn.addEventListener('click', () => {
+	  menu_btn.classList.remove('show');
+	  mobile_menu.classList.remove('show');
+	});
+
+	// Close the navbar menu when the toggle button is clicked
+	menu_btn.addEventListener('click', () => {
+	  if (!mobile_menu.classList.contains('show')) {
+	    mobile_menu.classList.add('show');
+	  } else {
+	    mobile_menu.classList.remove('show');
+	  }
+	});
+
+	// Close the navbar menu when the user clicks outside of it
+	document.addEventListener('click', (event) => {
+	  const target = event.target;
+	  if (!target.closest('.navbar') && mobile_menu.classList.contains('show')) {
+	    mobile_menu.classList.remove('show');
+	  }
+	});
+
+
 }
